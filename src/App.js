17d8bd6a3d5d5a22261375d7328279599eb5864a
Vincenius <vincet.will@vaitrade.de>
2020-04-23 16:54:42 +0200

    remove semicolon inconsistency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home'
@@ -12,7 +12,7 @@ function App() {
         <Route path = '/post/:id' render = {routerProps => <Post id={routerProps.match.params.id}/>} />
       </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
