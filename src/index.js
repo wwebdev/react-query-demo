d9d858fb853e68fa66167fe7e4ead4c4c6c0b369
Vincenius <vincet.will@vaitrade.de>
2020-04-24 10:20:00 +0200

    add react query dev tools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </React.StrictMode>,
   document.getElementById('root')
 );
